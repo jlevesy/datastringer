f4a9278a472cedb1e0243b41082656686d3f39dd
Julien Levesy <jlevesy@gmail.com>
2014-09-15 19:43:05 +0200

    platform: added custom_stringer dir, now enable user to deploy existing conf to container

---
+++ platform/docker_datastringer/custom_stringers/dummy_stringer.js
@@ -0,0 +1,7 @@
+
+function stringer(triggerAlertCallback) {
+  console.log('... OH WAIT WHAT ? ALERT !');
+  triggerAlertCallback('The Dummy Stringer', 'Huuu... What if I press this big red send-email button ?');
+};
+
+module.exports = stringer;
